796daff7741c1abd6aa317a50c9d097efc266f7e
Sudhir Thakurela <sudhir.thakurela@gmail.com>
2026-04-28 16:39:15 +0000

    fix: React Router basename + 404 redirect for GitHub Pages SPA routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/CLA">
       <App />
     </BrowserRouter>
-  </React.StrictMode>
-)
+  </Reac
